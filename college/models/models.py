b2092f1e3a455bbd4321d3f4a3f7e114285d52a0
unknown <yaseeno373@gmail.com>
2019-03-08 20:21:01 +0200

    init

---
+++ college/models/models.py
@@ -0,0 +1,22 @@
+# -*- coding: utf-8 -*-
+
+from odoo import models, fields, api
+
+class College(models.Model):
+    _name = 'college.college'
+
+    name = fields.Char('Name')
+    code = fields.Char(string="Code", required=False, )
+    dean_id = fields.Many2one(comodel_name="college.employee", string="Dean", required=False, )
+
+
+class Employee(models.Model):
+    _name = 'college.employee'
+
+    name = fields.Char('Name')
+    code = fields.Char('Code')
+    type = state = fields.Selection(string="", selection=[('academic', 'Academic'), ('admin', 'Administrative'),('workers','Workers') ], required=False, )
+    hiring_date = fields.Date(string="Hiring Date", required=False, )
+    collage_id =fields.Many2one(comodel_name="college.college", string="College", required=False, )
+
+
