598e5c8c2b42d298ffb5f041e8d5f892bbd844b9
sustodoo <odoosust@gmail.com>
2019-03-13 17:16:46 +0200

    add more js

--- college/static/src/js/college.js
+++ college/static/src/js/college.js
@@ -1,26 +1,42 @@
 odoo.define('college.tour', function(require) {
-"use strict";
+    "use strict";
 
-var Widget = require('web.Widget');
-var core = require('web.core');
-var Model = require('web.rpc');
-
-
- var HomePage = Widget.extend({
+    var Widget = require('web.Widget');
+    var core = require('web.core');
+    var Model = require('web.rpc');
+    var ajax = require('web.ajax');
+    var HomePage = Widget.extend({
+        template: "HomePage",
         start: function() {
-            var products = new ProductsWidget(
-                this, ["cpu", "mouse", "keyboard", "graphic card", "screen"], "#00FF00");
-            products.appendTo(this.$el);
+            this.colorInput = new ColorInputWidget(this);
+            this.colorInput.on("change:color", this, this.color_changed);
+            return this.colorInput.appendTo(this.$el);
+        },
+        color_changed: function() {
+            this.$(".oe_color_div").css("background-color", this.colorInput.get("color"));
         },
     });
 
-    var ProductsWidget = Widget.extend({
-        template: "ProductsWidget",
-        init: function(parent, products, color) {
-            this._super(parent);
-            this.products = products;
-            this.color = color;
+
+    var ColorInputWidget = Widget.extend({
+        template: "ColorInputWidget",
+        events: {
+            'change input': 'input_changed'
+        },
+        start: function() {
+            this.input_changed();
+            return this._super();
+        },
+        input_changed: function() {
+            var color = [
+            "#",
+            this.$(".oe_color_red").val(),
+            this.$(".oe_color_green").val(),
+            this.$(".oe_color_blue").val()
+            ].join('');
+            this.set("color", color);
         },
     });
+
     core.action_registry.add('ProductsWidget', HomePage); 
 });
